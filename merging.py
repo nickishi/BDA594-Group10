f65a5dc4f8328f3438208f8cf378b0d2ce2f9add
Marrion <mmacandog7304@sdsu.edu>
2021-10-28 14:31:20 -0700

    recoded dv and checked imbalanced data

--- merging.py
+++ merging.py
@@ -2,6 +2,7 @@ import pandas as pd
 import seaborn as sn
 import matplotlib.pyplot as plt
 import numpy as np
+import sklearn
 
 df = pd.read_csv(r'/Users/marrionmac/PycharmProjects/bda_594/triage.csv')
 df2 = pd.read_csv(r'/Users/marrionmac/PycharmProjects/bda_594/diagnosis.csv')
@@ -30,15 +31,21 @@ corrMatrix = round(df.corr(),2)
 sn.heatmap(corrMatrix, annot=True)
 plt.show() # no significant ( > 0.8) correlations yay!
 
-# label dependent/independent variables
+# recoding dependent variable 'acuity'
 # acuity considered the classifier, 1:2=need medical attention, 3:5=not as severe
-dv = df.acuity # dependent variable
-ivs = df.loc[:, df.columns != 'acuity'] # all other columns as independent variables
+df["acuity"].replace({2: 1, 3: 0, 4: 0, 5: 0}, inplace=True)
 
-sn.scatterplot(x=df['temperature'], y=dv)
-plt.show()
+# check if dependent variable is impartial
+total_count = df.shape[0]
+not_severe = df[df.acuity == 0].shape[0]
+severe = df[df.acuity == 1].shape[0]
+severe_Ratio = severe/total_count
+not_severe_Ratio = not_severe/total_count
+print("Ratio of severe:", severe_Ratio)
+print("Ratio of not severe:", not_severe_Ratio)
+## the data is imbalanced...
 
-# split data, 30% training data
+
 
 
 
