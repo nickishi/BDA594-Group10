11a708c064da7f0e08a7e234d36f2986ba9f96d0
Marrion <mmacandog7304@sdsu.edu>
2021-12-05 11:00:26 -0800

    logistic regression + performance

--- BDA_594_ANALYSIS.R
+++ BDA_594_ANALYSIS.R
@@ -7,8 +7,8 @@ require(caret)
 
 ######### analyses ######### 
 
-#data <- read.csv("~/cleaned_data2.csv", header=TRUE)
-data <- read.csv("cleaned_data2.txt")
+data <- read.csv("~/cleaned_data5.csv", header=TRUE)
+#data <- read.csv("cleaned_data2.txt")
 View(data)
 
 # split data into training and test set
@@ -20,6 +20,10 @@ data$acuity[data$acuity == 1] <- "Yes"
 
 # create factor of 2 levels
 data$acuity <- as.factor(data$acuity)
+
+# relevel the yes category as the positive class
+data$acuity <- relevel(data$acuity, ref="Yes")
+
 
 set.seed(199)
 trainIndex <- createDataPartition(data$acuity, p = .7, list=F)
@@ -33,7 +37,7 @@ prop.table(d.table)
 
 #setup control function for resampling and binary classification performance
 #using 10 fold cross validation
-ctrl <- trainControl(method = "cv", number=5, summaryFunction=twoClassSummary,
+ctrl <- trainControl(method = "cv", number=10, summaryFunction=twoClassSummary,
                      classProbs=T, savePredictions=T) #saving predictions from each resample fold
 
 ##logistic regression
@@ -42,49 +46,53 @@ d.log <-  train(acuity ~ ., data=train_data, method="glm", family="binomial", me
 summary(d.log)
 varImp(d.log)
 d.log
+d.log$finalModel
 
 #calculate resampled accuracy/confusion matrix using extracted predictions from resampling
 confusionMatrix(d.log$pred$pred, d.log$pred$obs) #take averages
 
-##linear discriminant analysis
-set.seed(199)
-d.lda <-  train(acuity ~ ., data=train_data, method="lda", family="binomial", metric="ROC", trControl=ctrl)
-d.lda
-varImp(d.lda)
-confusionMatrix(d.lda$pred$pred, d.lda$pred$obs) #take averages
-
-#k nearest neighbors classification
-set.seed(199) 
-d.knn <-  train(acuity ~ ., data=train_data, method="knn", metric="ROC", trControl=ctrl, tuneLength=3) #let caret decide 10 best parameters to search
-d.knn
-plot(d.knn)
-getTrainPerf(d.knn)
-
-confusionMatrix(d.knn$pred$pred, d.knn$pred$obs) #make sure to select resamples only for optimal parameter of K
-
 #really need test set to get more accurate idea of accuracy when their is a rare class
 #can either use model on cross validation of complete training data or hold out test set
 
-#lets compare all resampling approaches
-d.models <- list("logit"=d.log, "lda"=d.lda,
-                 "knn"=d.knn)
-d.resamples = resamples(d.models)
-
-
-#plot performance comparisons
-bwplot(d.resamples, metric="ROC") 
-bwplot(d.resamples, metric="Sens") #predicting default dependant on threshold
-bwplot(d.resamples, metric="Spec") 
 
 #calculate ROC curves on resampled data
 
 d.log.roc<- roc(response= d.log$pred$obs, predictor=d.log$pred$Yes)
-d.lda.roc<- roc(response= d.lda$pred$obs, predictor=d.lda$pred$Yes)
-#when model has parameters make sure to select final parameter value
-d.knn.roc<- roc(response= d.knn$pred[d.knn$pred$k==23,]$obs, predictor=d.knn$pred[d.knn$pred$k==23,]$Yes) 
 
 #build to combined ROC plot with resampled ROC curves
 plot(d.log.roc, legacy.axes=T)
-plot(d.lda.roc, add=T, col="Blue")
-plot(d.knn.roc, add=T, col="Red")
-legend(x=.2, y=.7, legend=c("Logit", "LDA", "QDA", "KNN"), col=c("black","blue","green","red"),lty=1)
+
+#extract threshold from roc curve  get threshold at coordinates top left most corner
+d.log.Thresh<- coords(d.log.roc, x="best", best.method="closest.topleft")
+d.log.Thresh #sensitivity increases to 88% by reducing threshold to .0396 from .5
+
+
+### TEST DATA ### 
+# LOGISTIC REGRESSION
+
+#predict probabilities on test set with log trained model
+d.test <- test_data
+
+test.pred.prob <- predict(d.log, d.test, type="prob")
+
+test.pred.class <- predict(d.log, d.test) #predict classes with default .5 cutoff
+
+#calculate performance with confusion matrix
+confusionMatrix(test.pred.class, d.test$acuity)
+
+#let draw ROC curve of training and test performance of logit model
+test.log.roc<- roc(response= d.test$acuity, predictor=test.pred.prob[[1]]) #assumes postive class Yes is reference level
+plot(test.log.roc, legacy.axes=T)
+plot(d.log.roc, add=T, col="blue")
+legend(x=.2, y=.7, legend=c("Test Logit", "Train Logit"), col=c("black", "blue"),lty=1)
+
+#test performance slightly lower than resample
+auc(test.log.roc)
+auc(d.log.roc)
+
+#calculate test confusion matrix using thresholds from resampled data
+test.pred.class.newthresh <- factor(ifelse(test.pred.prob[[1]] > d.log.Thresh[1], "Yes", "No"))
+
+#recalculate confusion matrix with new cut off predictions
+confusionMatrix(test.pred.class.newthresh, d.test$acuity)
+
